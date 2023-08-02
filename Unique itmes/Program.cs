3627914a52c0aa64492c113dedcf6418162c8fa2
AMIR <AMIR@DESKTOP-3I9AEUC.(none)>
2023-08-02 12:39:42 +0330

    Add project files.

---
+++ Unique itmes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Unique_itmes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" (Split numbers with Space) \n \n Enter the Numbers : ");
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            
+            int uniquecount = GetNumberOfUniqueElements(array);
+            Console.WriteLine("\n Number of Unique Items :  " + uniquecount);
+            
+            static int GetNumberOfUniqueElements(int[] array)
+            {
+                
+                HashSet<int> uniqueElements = new HashSet<int>();
+                
+                foreach (int element in array)
+                {
+                    
+                    uniqueElements.Add(element);
+                }
+                // Return the number of unique elements in the set.
+                return uniqueElements.Count;
+            }
+
+
+
+
+        }
+    }
+}
